7c0bf8a19a180c6aafd3adf73a38e46279ade428
anurag2787 <anuragyadav2787@gmail.com>
2024-09-30 13:09:58 +0530

    footer fixex

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear(); // Gets the current year
 
   return (
-    <footer className="bg-black text-white text-center py-4 bottom-0  w-full md:fixed">
+    <footer className="bg-black text-white text-center py-4 bottom-0  w-full fixed">
       <p className="text-sm">
         &copy; {year} Developer Anurag Yadav. All rights reserved.
       </p>
