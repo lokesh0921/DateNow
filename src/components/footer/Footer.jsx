00a9139c9ea9626e42202f2982bc68141d572da5
anurag2787 <anuragyadav2787@gmail.com>
2024-09-30 13:15:27 +0530

    footer overlap fixes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   const year = new Date().getFullYear(); // Gets the current year
 
   return (
-    <footer className="bg-black text-white text-center py-4 bottom-0  w-full fixed">
-      <p className="text-sm">
+    <footer className="bg-black text-white text-center py-4 bottom-0   w-full md:fixed pb-[env(safe-area-inset-bottom)]">
+      <p className="text-sm pb-3">
         &copy; {year} Developer Anurag Yadav. All rights reserved.
       </p>
     </footer>
