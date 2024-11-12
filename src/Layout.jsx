d4d45368ed48d83a8bfa05db57347bb0b308d3f5
anurag2787 <anuragyadav2787@gmail.com>
2024-11-12 16:33:01 +0530

    added vercel analytics

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,11 +3,13 @@ import Navbar from './components/navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Cursor from './components/Cursor/Cursor'
+import { Analytics } from "@vercel/analytics/react"
 
 
 function Layout() {
   return (
     <>
+    <Analytics/>
     <Navbar/>
     <Cursor />
     <Outlet />
