599a5f97018080d0378ee3227747ecd0835984c4
anurag2787 <anuragyadav2787@gmail.com>
2024-11-16 03:01:52 +0530

    Added custom navbar for mobile devices witha menu button it will open the navbar on clciking the menu button and enhance the ui of the page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,51 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import menu from "../../assets/menu.svg";
 
 function Navbar() {
+  const [view, setView] = useState(true);
+
+  const mobileView = () => {
+    console.log("clicked");
+    setView(false);
+  };
+  const dekstopView = () => {
+    setView(true);
+  };
+
   return (
-    <div className="bg-black w-full h-[60px] left-0 flex items-center px-4 right-0 top-0 text-base  md:text-xl sticky">
-      <ul className="flex items-center text-white space-x-10 ml-6 w-full">
-        <li className="relative group">
+    <>
+      {view ? (
+        <div className=" bg-black w-full h-[50px] max-h-[100px] md:h-[60px] left-0 flex items-center px-4 right-0 top-0 text-base md:text-xl sticky">
+          <div className="md:hidden text-xl mx-auto">
+            <span className="text-white font-bold space-x-2">Date</span>
+            <span className="mx-[2px] text-red-500 font-bold">Now</span>
+          </div>
+          <div className="md:hidden flex w-12 top-1 right-2 absolute" onClick={mobileView}>
+            <img src={menu} alt="Menu" />
+          </div>
+          {/* Desktop View Navbar */}
+          <ul className="hidden md:flex items-center text-white space-x-10 ml-6 w-full">
+            <li className="relative group">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "border-white" : "border-transparent"
+                  } border-transparent border-b-2 transition-all p-1 duration-300 ease-in`
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="relative group">
+              <NavLink
+                to="/contact"
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "border-white" : "border-transparent"
+                  } border-transparent border-b-2 p-1 transition-all duration-300 ease-in`
+                }
+              >
+                Contact Us
+              </NavLink>
+            </li>
+            <li className="relative group">
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "border-white" : "border-transparent"
+                  } border-transparent border-b-2 p-1 transition-all duration-300 ease-in`
+                }
+              >
+                About Us
+              </NavLink>
+            </li>
+          </ul>
+          <div className="hidden md:block text-2xl">
+            <span className="text-white font-bold space-x-2">Date</span>
+            <span className="mx-[2px] text-red-500 font-bold">Now</span>
+          </div>
+        </div>
+      ) : (
+        <div className="bg-black w-full max-h-[400px]  md:h-[60px] left-0 flex flex-col items-center px-4 text-base text-white ">
+          <div className="text-xl mx-auto my-4">
+            <span className="text-white font-bold space-x-2">Date</span>
+            <span className="mx-[2px] text-red-500 font-bold">Now</span>
+          </div>
+          <div
+            className="flex w-12 top-1 right-2 absolute"
+            onClick={dekstopView}
+          >
+            <img src={menu} alt="Menu" />
+          </div>
+
           <NavLink
             to="/"
             className={({ isActive }) =>
-              `  ${
+              `${
                 isActive ? "border-white" : "border-transparent"
-              } border-transparent border-b-2 transition-all p-1 duration-300 ease-in`
+              } border-transparent border-b-2 transition-all p-1 duration-300 ease-in my-3`
             }
           >
             Home
           </NavLink>
-        </li>
-        <li className="relative group">
           <NavLink
             to="/contact"
             className={({ isActive }) =>
-              `  ${
+              `${
                 isActive ? "border-white" : "border-transparent"
-              } border-transparent border-b-2 p-1 transition-all duration-300 ease-in`
+              } border-transparent border-b-2 p-1 transition-all duration-300 ease-in my-3 `
             }
           >
             Contact Us
           </NavLink>
-        </li>
-        <li className="relative group">
           <NavLink
             to="/about"
             className={({ isActive }) =>
-              `  ${
+              `${
                 isActive ? "border-white" : "border-transparent"
-              } border-transparent border-b-2 p-1 transition-all duration-300 ease-in`
+              } border-transparent border-b-2 p-1 transition-all duration-300 ease-in my-3 mb-7`
             }
           >
             About Us
           </NavLink>
-        </li>
-      </ul>
-      <div className="hidden md:block text-2xl">
-        <span className="text-white  font-bold space-x-2">Date</span><span className="mx-[2px] text-red-500  font-bold">Now</span>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
 
