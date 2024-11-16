84e6905b4cec2a3bd6ea603d0dff88c5b1abae07
anurag2787 <anuragyadav2787@gmail.com>
2024-11-16 19:22:27 +0530

    Improved mobile view menu

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -61,7 +61,7 @@ export default function Contact() {
  
 
   return (
-    <div className=" relative flex items-top justify-center min-h-[700px] md:min-h-screen bg-[#F8A199] sm:items-center sm:pt-0 md:mx-0 mx-5 mb-8">
+    <div className="flex items-top justify-center min-h-[700px] md:min-h-screen bg-[#F8A199] sm:items-center sm:pt-0 md:mx-0 mx-5 mb-8">
       <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
         <div className="mt-8 overflow-hidden">
           {/* <div className=" grid grid-cols-1 md:grid-cols-2 bg-[#ffdad7] p-8 rounded-lg"> */}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,43 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import menu from "../../assets/menu.svg";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 function Navbar() {
   const [view, setView] = useState(true);
 
+
+
   const mobileView = () => {
-    console.log("clicked");
-    setView(false);
+    // Ensure the initial state is correct (scale 0, off-screen)
+    const tl=gsap.timeline();
+    gsap.set(".mbmenu", {
+      scale: 1,
+      top: -300,
+    });
+  
+    // Animate the menu to its final position
+    tl.to(".mbmenu", {
+      scale: 1,
+      top: 0,
+      duration: 0.7,
+    });
+  
+    setView(true); // Set state to show the mobile menu
   };
-  const dekstopView = () => {
-    setView(true);
+  
+  const rollback = () => {
+    gsap.to(".mbmenu", {
+      scale: 1,
+      top:-300,
+      duration: 0.7,
+    });
   };
 
   return (
     <>
-      {view ? (
+      {/* {view ? ( */}
         <div className=" bg-black w-full h-[50px] max-h-[100px] md:h-[60px] left-0 flex items-center px-4 right-0 top-0 text-base md:text-xl sticky">
           <div className="md:hidden text-xl mx-auto">
             <span className="text-white font-bold space-x-2">Date</span>
@@ -68,21 +90,22 @@ function Navbar() {
             <span className="mx-[2px] text-red-500 font-bold">Now</span>
           </div>
         </div>
-      ) : (
-        <div className="bg-black w-full max-h-[400px]  md:h-[60px] left-0 flex flex-col items-center px-4 text-base text-white sticky">
+      {/* ) : ( */}
+        <div className="mbmenu  md:hidden top-[-300px] absolute bg-black w-full max-h-[400px]  md:h-[60px] left-0 flex flex-col items-center px-4 text-base text-white">
           <div className="text-xl mx-auto my-4">
             <span className="text-white font-bold space-x-2">Date</span>
             <span className="mx-[2px] text-red-500 font-bold">Now</span>
           </div>
           <div
             className="flex w-12 top-1 right-2 absolute"
-            onClick={dekstopView}
+            onClick={rollback}
           >
             <img src={menu} alt="Menu" />
           </div>
 
           <NavLink
             to="/"
+            onClick={rollback}
             className={({ isActive }) =>
               `${
                 isActive ? "border-white" : "border-transparent"
@@ -93,6 +116,7 @@ function Navbar() {
           </NavLink>
           <NavLink
             to="/contact"
+            onClick={rollback}
             className={({ isActive }) =>
               `${
                 isActive ? "border-white" : "border-transparent"
@@ -103,6 +127,7 @@ function Navbar() {
           </NavLink>
           <NavLink
             to="/about"
+            onClick={rollback}
             className={({ isActive }) =>
               `${
                 isActive ? "border-white" : "border-transparent"
@@ -112,7 +137,7 @@ function Navbar() {
             About Us
           </NavLink>
         </div>
-      )}
+      {/* )} */}
     </>
   );
 }
